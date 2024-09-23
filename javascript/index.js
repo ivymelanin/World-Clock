73a92ef723944186141c8f703f18479aa208fb21
ivymelanin <leratokhauta20@gmail.com>
2024-09-24 00:59:31 +0200

    imported moment js and added some fuction in the js file and updated the index and css file

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+function updateTime() {
+
+    //los angeles
+    let losAngeles = document.querySelector("#losAngeles");
+    let losAngelesDate = losAngeles.querySelector(".date");
+    let losAngelesTime = losAngeles.querySelector(".time");
+    let currentTime = moment().tz("America/Los_Angeles");
+
+    losAngelesDate.innerHTML = currentTime.format("MMMM Do YYYY");
+    losAngelesTime.innerHTML = currentTime.form("h:mm:ss [<small>]A[</small>");
+
+    // sydeny
+    let sydney = document.querySelector("#sydney");
+    let sydneyDate = sydney.querySelector(".date");
+    let sydneyTime = sydney.querySelector(".time");
+    let currentTime1 = moment().tz("Australia/Sydney");
+
+    sydneyDate.innerHTML = currentTime1.format("MMMM Do YYYY");
+    sydneyTime.innerHTML = currentTime1.form("h:mm:ss [<small>]A[</small>");
+
+    //new york
+    let newYork = document.querySelector("#newYork");
+    let newYorkDate = newYork.querySelector(".date");
+    let newYorkTime = newYork.querySelector(".time");
+    let currentTime2 = moment().tz("America/New_York");
+
+    newYorkDate.innerHTML = currentTime2.format("MMMM Do YYYY");
+    newYorkTime.innerHTML = currentTime2.form("h:mm:ss [<small>]A[</small>");
+
+}
+
+updateTime();
+setInterval(updateTime, 1000);
